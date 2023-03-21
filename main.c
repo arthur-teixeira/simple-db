597b7b12ccbc4ea748c74651f07fb36b7ee42fdb
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-03-21 19:38:26 -0300

    fix: Fix table max row count

--- main.c
+++ main.c
@@ -69,7 +69,7 @@ void deserialize_row(void *source, Row *destination) {
 const uint32_t PAGE_SIZE = 4096; // default page size in the linux kernel
 #define TABLE_MAX_PAGES 100
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * PAGE_SIZE * TABLE_MAX_PAGES;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 typedef struct {
   uint32_t num_rows;
