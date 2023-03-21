e0c2c7220878ddd0209a4c119f26449f9e967f64
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-03-21 19:38:43 -0300

    test: Added table full error test

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,10 +1,8 @@
-describe 'database' do
+describe "database" do
   def run_script(commands)
     raw_output = nil
     IO.popen("./db", "r+") do |pipe|
-      commands.each do |command|
-        pipe.puts command
-      end
+      commands.each { |command| pipe.puts command }
       pipe.close_write
 
       raw_output = pipe.gets(nil)
@@ -12,18 +10,24 @@ describe 'database' do
     raw_output.split("\n")
   end
 
-  it 'Inserts and retrieves a row' do
-    result = run_script([
-      "insert 1 user1 person1@example.com",
-      "select",
-      ".exit",
-    ])
+  it "Inserts and retrieves a row" do
+    result =
+      run_script(["insert 1 user1 person1@example.com", "select", ".exit"])
 
-    expect(result).to match_array([
-      "db > Executed.",
-      "db > (1, user1, person1@example.com)",
-      "Executed.",
-      "db > ",
-    ])
+    expect(result).to match_array(
+      [
+        "db > Executed.",
+        "db > (1, user1, person1@example.com)",
+        "Executed.",
+        "db > "
+      ]
+    )
+  end
+
+  it "prints error message when table is full" do
+    script = (1..1401).map { |i| "insert #{i} user#{i} person#{i}@example.com" }
+    script << ".exit"
+    result = run_script(script)
+    expect(result[-2]).to eq("db > Error: Table full.")
   end
 end
